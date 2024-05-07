c909c629f0af7ce08bdc471eaa316c3c1cf04efb
ArifShahriar028 <arifshahriar028@gmail.com>
2024-05-07 18:06:46 +0600

    initial agent creation

---
+++ index.ts
@@ -0,0 +1,47 @@
+
+// core agent modules
+import type { InitConfig } from '@aries-framework/core';
+import { Agent } from '@aries-framework/core';
+import { agentDependencies } from '@aries-framework/node';
+
+// askar modules
+import { AskarModule } from '@aries-framework/askar'
+import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
+
+// agent outbound transport modules to handle traffic from the agent
+import { HttpOutboundTransport, WsOutboundTransport } from '@aries-framework/core'
+import { HttpInboundTransport } from '@aries-framework/node'
+
+const config: InitConfig = {
+  label: 'docs-agent-nodejs',
+  walletConfig: {
+    id: 'wallet-id',
+    key: 'testkey0000000000000000000000000',
+  },
+};
+
+const agent = new Agent({
+  config,
+  dependencies: agentDependencies,
+  modules: {
+    // Register the Askar module on the agent
+    askar: new AskarModule({
+      ariesAskar,
+    }),
+  },
+})
+
+
+agent.registerOutboundTransport(new HttpOutboundTransport())
+agent.registerOutboundTransport(new WsOutboundTransport())
+agent.registerInboundTransport(new HttpInboundTransport({ port: 3000 }))
+
+
+agent
+  .initialize()
+  .then(() => {
+    console.log('Agent initialized!')
+  })
+  .catch((e) => {
+    console.error(`Something went wrong while setting up the agent! Message: ${e}`)
+  })
