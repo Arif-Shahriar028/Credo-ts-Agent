bcb063305d2ff30919c92fcc984da01faae00c74
ArifShahriar028 <arifshahriar028@gmail.com>
2024-05-08 12:40:57 +0600

    initial agent setup

--- index.ts
+++ index.ts
@@ -1,47 +1,161 @@
-
-// core agent modules
-import type { InitConfig } from '@aries-framework/core';
-import { Agent } from '@aries-framework/core';
-import { agentDependencies } from '@aries-framework/node';
-
-// askar modules
 import { AskarModule } from '@aries-framework/askar'
+import {
+  Agent,
+  InitConfig,
+  ConnectionEventTypes,
+  ConnectionStateChangedEvent,
+  WsOutboundTransport,
+  HttpOutboundTransport,
+  DidExchangeState,
+  OutOfBandRecord,
+  ConnectionsModule,
+} from '@aries-framework/core'
+import { agentDependencies, HttpInboundTransport } from '@aries-framework/node'
 import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
 
-// agent outbound transport modules to handle traffic from the agent
-import { HttpOutboundTransport, WsOutboundTransport } from '@aries-framework/core'
-import { HttpInboundTransport } from '@aries-framework/node'
 
-const config: InitConfig = {
-  label: 'docs-agent-nodejs',
-  walletConfig: {
-    id: 'wallet-id',
-    key: 'testkey0000000000000000000000000',
-  },
-};
+//** Initialization of bob agent
 
-const agent = new Agent({
-  config,
-  dependencies: agentDependencies,
-  modules: {
-    // Register the Askar module on the agent
-    askar: new AskarModule({
-      ariesAskar,
-    }),
-  },
-})
+const initializeBobAgent = async () => {
+  // Simple agent configuration. This sets some basic fields like the wallet
+  // configuration and the label. It also sets the mediator invitation url,
+  // because this is most likely required in a mobile environment.
+  const config: InitConfig = {
+    label: 'demo-agent-bob',
+    walletConfig: {
+      id: 'mainBob',
+      key: 'demoagentbob00000000000000000000',
+    },
+  }
 
-
-agent.registerOutboundTransport(new HttpOutboundTransport())
-agent.registerOutboundTransport(new WsOutboundTransport())
-agent.registerInboundTransport(new HttpInboundTransport({ port: 3000 }))
-
-
-agent
-  .initialize()
-  .then(() => {
-    console.log('Agent initialized!')
+  // A new instance of an agent is created here
+  // Askar can also be replaced by the indy-sdk if required
+  const agent = new Agent({
+    config,
+    modules: {
+      askar: new AskarModule({ ariesAskar }),
+      connections: new ConnectionsModule({ autoAcceptConnections: true }),
+    },
+    dependencies: agentDependencies,
   })
-  .catch((e) => {
-    console.error(`Something went wrong while setting up the agent! Message: ${e}`)
+
+  // Register a simple `WebSocket` outbound transport
+  agent.registerOutboundTransport(new WsOutboundTransport())
+
+  // Register a simple `Http` outbound transport
+  agent.registerOutboundTransport(new HttpOutboundTransport())
+
+  // Register a simple `Http` inbound transport
+  // agent.registerInboundTransport(new HttpInboundTransport({ port: 3002 }))
+
+  
+
+  // Initialize the agent
+  await agent.initialize()
+
+  return agent
+}
+
+const initializeAcmeAgent = async () => {
+  // Simple agent configuration. This sets some basic fields like the wallet
+  // configuration and the label.
+  const config: InitConfig = {
+    label: 'demo-agent-acme',
+    walletConfig: {
+      id: 'mainAcme',
+      key: 'demoagentacme0000000000000000000',
+    },
+    endpoints: ['http://localhost:3001'],
+  }
+
+  // A new instance of an agent is created here
+  // Askar can also be replaced by the indy-sdk if required
+  const agent = new Agent({
+    config,
+    modules: {
+      askar: new AskarModule({ ariesAskar }),
+      connections: new ConnectionsModule({ autoAcceptConnections: true }),
+    },
+    dependencies: agentDependencies,
   })
+
+  // console.log(typeof(Agent))
+
+  // Register a simple `WebSocket` outbound transport
+  agent.registerOutboundTransport(new WsOutboundTransport())
+
+  // Register a simple `Http` outbound transport
+  agent.registerOutboundTransport(new HttpOutboundTransport())
+
+  // Register a simple `Http` inbound transport
+  agent.registerInboundTransport(new HttpInboundTransport({ port: 3001 }))
+
+  // Initialize the agent
+  await agent.initialize()
+
+  return agent
+}
+
+const createNewInvitation = async (agent: Agent) => {
+  const outOfBandRecord = await agent.oob.createInvitation()
+
+  return {
+    invitationUrl: outOfBandRecord.outOfBandInvitation.toUrl({ domain: 'https://example.org' }),
+    outOfBandRecord,
+  }
+}
+
+const createLegacyInvitation = async (agent: Agent) => {
+  const { invitation } = await agent.oob.createLegacyInvitation()
+
+  return invitation.toUrl({ domain: 'https://example.org' })
+}
+
+const receiveInvitation = async (agent: Agent, invitationUrl: string) => {
+  const { outOfBandRecord } = await agent.oob.receiveInvitationFromUrl(invitationUrl)
+
+  return outOfBandRecord
+}
+
+const setupConnectionListener = (agent: Agent, outOfBandRecord: OutOfBandRecord, cb: (...args: any) => void) => {
+  agent.events.on<ConnectionStateChangedEvent>(ConnectionEventTypes.ConnectionStateChanged, ({ payload }) => {
+    if (payload.connectionRecord.outOfBandId !== outOfBandRecord.id) return
+    if (payload.connectionRecord.state === DidExchangeState.Completed) {
+      // the connection is now ready for usage in other protocols!
+      console.log(`Connection for out-of-band id ${outOfBandRecord.id} completed`)
+
+      // Custom business logic can be included here
+      // In this example we can send a basic message to the connection, but
+      // anything is possible
+      cb()
+
+      // We exit the flow
+      process.exit(0)
+    }
+  })
+}
+
+
+const run = async () => {
+  console.log('Initializing Bob agent...')
+  const bobAgent = await initializeBobAgent()
+  console.log('Initializing Acme agent...')
+  const acmeAgent = await initializeAcmeAgent()
+
+  // console.log(typeof(acmeAgent));
+
+  console.log('Creating the invitation as Acme...')
+  const { outOfBandRecord, invitationUrl } = await createNewInvitation(acmeAgent)
+
+  console.log('Listening for connection changes...')
+  setupConnectionListener(acmeAgent, outOfBandRecord, () =>
+    console.log('We now have an active connection to use in the following tutorials')
+  )
+
+  console.log('Accepting the invitation as Bob...')
+  await receiveInvitation(bobAgent, invitationUrl)
+}
+
+export default run
+
+void run()
