98946dad7d96d2744f168cc75261bca03eef338f
ArifShahriar028 <arifshahriar028@gmail.com>
2024-05-16 12:18:51 +0600

    Readme updated

--- src/holder/credential-listener.ts
+++ src/holder/credential-listener.ts
@@ -14,18 +14,15 @@ const setUpCredentialListener = (holder: Agent)=>{
     console.log(">>>>  " + payload.credentialRecord.state)
 
     if(payload.credentialRecord.state === CredentialState.OfferReceived){
-      console.log('received a credential, credential id: '+ payload.credentialRecord.id)
       await holder.credentials.acceptOffer({ credentialRecordId: payload.credentialRecord.id })
     } 
     if(payload.credentialRecord.state === CredentialState.CredentialReceived){
-      console.log('Credential received')
       await holder.credentials.acceptCredential({ credentialRecordId: payload.credentialRecord.id })
     }
     else if(payload.credentialRecord.state === CredentialState.Done){
-      console.log(`Credential for credential id ${payload.credentialRecord.id} is accepted`)
-      console.log('credential state : ', payload.credentialRecord.state)
-      console.log('Comparing parameter', CredentialState.Done)
+      console.log("Full Credential record")
       console.log(payload.credentialRecord)
+      console.log(`Credential for credential id ${payload.credentialRecord.id} is accepted`)
     }
   })
 }
@@ -33,16 +30,3 @@ const setUpCredentialListener = (holder: Agent)=>{
 
 export default setUpCredentialListener
 
-
-
-// ProposalSent = "proposal-sent",
-// ProposalReceived = "proposal-received",
-// OfferSent = "offer-sent",
-// OfferReceived = "offer-received",
-// Declined = "declined",
-// RequestSent = "request-sent",
-// RequestReceived = "request-received",
-// CredentialIssued = "credential-issued",
-// CredentialReceived = "credential-received",
-// Done = "done",
-// Abandoned = "abandoned"
